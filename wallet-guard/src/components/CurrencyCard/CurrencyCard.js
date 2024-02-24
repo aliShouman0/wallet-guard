f507a131677bf7235b8d3131b5d8407ba6341e55
Ali Alrida Shouman <alishouman010@gmail.com>
2024-02-24 14:04:20 +0200

    Creating CurrencyCard component

---
+++ wallet-guard/src/components/CurrencyCard/CurrencyCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { View } from "react-native";
+import colors from "../../themes/colors";
+
+const CurrencyCard = ({data,onClick}) => {
+  return <View></View>;
+};
+
+CurrencyCard.propTypes = {};
+// defaultProps
+export default CurrencyCard;
