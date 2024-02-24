5cd89087b55f9cca69b723f1de74c7d003af3cdd
Ali Alrida Shouman <alishouman010@gmail.com>
2024-02-24 14:04:49 +0200

    Adding screen title

--- wallet-guard/src/screens/Dashboard/Dashboard.js
+++ wallet-guard/src/screens/Dashboard/Dashboard.js
@@ -1,12 +1,30 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { SafeAreaView, StatusBar, Text, View } from "react-native";
-import { Entypo } from "@expo/vector-icons";
+import { SafeAreaView, ScrollView, StatusBar, Text, View } from "react-native";
+
+import colors from "../../themes/colors";
+import fontSize from "../../themes/fontSize";
+import CurrencyCard from "../../components/CurrencyCard/CurrencyCard";
 
 const Dashboard = (props) => {
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={"light-content"} backgroundColor={"#195CE5FF"} />
+    <SafeAreaView
+      style={{ backgroundColor: colors.white, flex: 1, padding: 17 }}
+    >
+      <StatusBar barStyle={"light-content"} backgroundColor={colors.appBlue} />
+      <Text
+        style={{
+          fontSize: fontSize.XXLarge,
+          fontWeight: "bold",
+        }}
+      >
+        Select currency
+      </Text>
+      <ScrollView style={{}}>
+        <CurrencyCard data={data} onClick={() => {}} />
+        {/* <CurrencyCard />
+        <CurrencyCard />
+        <CurrencyCard /> */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
