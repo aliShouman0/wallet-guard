a98754050f05a14ff24ec1436655cb8f9615a160
Ali Alrida Shouman <alishouman010@gmail.com>
2024-02-28 14:49:42 +0200

    Fixing styles

--- wallet-guard/src/components/CurrencyCard/CurrencyCard.js
+++ wallet-guard/src/components/CurrencyCard/CurrencyCard.js
@@ -1,12 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View } from "react-native";
-import colors from "../../themes/colors";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const CurrencyCard = ({data,onClick}) => {
-  return <View></View>;
+import fontSize from "../../themes/fontSize";
+import HorizontalLine from "../HorizontalLine/HorizontalLine";
+import { AntDesign } from "@expo/vector-icons";
+
+const CurrencyCard = ({ data, onClick }) => {
+  return (
+    <>
+      <TouchableOpacity style={styles.root} onPress={onClick}>
+        <View>
+          <Text style={styles.mainText}>{data.currency}</Text>
+          <Text style={styles.subText}>{data.currencyCountry}</Text>
+        </View>
+        <AntDesign name="right" size={24} color="black" />
+      </TouchableOpacity>
+
+      <HorizontalLine borderStyle={"solid"} opacity={0.1} />
+    </>
+  );
 };
 
-CurrencyCard.propTypes = {};
+const styles = StyleSheet.create({
+  root: {
+    padding: 17,
+    paddingTop: 10,
+    paddingBottom: 10,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    height: 60,
+    elevation: 2,
+    borderBottomWidth: 2,
+    borderColor: "transparent",
+  },
+  mainText: { fontSize: fontSize.XLarge, fontWeight: "bold" },
+  subText: { fontSize: fontSize.small, fontWeight: "400" },
+});
+
+CurrencyCard.propTypes = {
+  data: PropTypes.shape({
+    currency: PropTypes.string,
+    currencyCountry: PropTypes.string,
+  }),
+};
+
 // defaultProps
 export default CurrencyCard;
