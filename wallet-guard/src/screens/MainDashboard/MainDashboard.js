8668d88ead0118aaf3f4cb808996a3757817a931
Ali Alrida Shouman <alishouman010@gmail.com>
2024-03-01 00:40:28 +0200

    Styling Top container

--- wallet-guard/src/screens/MainDashboard/MainDashboard.js
+++ wallet-guard/src/screens/MainDashboard/MainDashboard.js
@@ -1,14 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 import colors from "../../themes/colors";
+import fontSize from "../../themes/fontSize";
 
 const MainDashboard = (props) => {
   return (
     <SafeAreaView style={styles.root}>
       <StatusBar barStyle={"light-content"} backgroundColor={colors.appBlue} />
-      {/* <Text style={styles.text}>Select currency</Text> */}
-      <ScrollView style={{ flex: 1, gap: 2 }}></ScrollView>
+      <ScrollView style={{ flex: 1, gap: 2 }}>
+        <View style={styles.balanceContainer}>
+          <View>
+            <Text style={styles.text}>Account Balance</Text>
+            <Text style={styles.boldText}>5,943$</Text>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -17,7 +31,14 @@ const styles = StyleSheet.create({
   root: {
     backgroundColor: colors.white,
     flex: 1,
-    paddingTop: 0,
+    padding: 10,
+  },
+  balanceContainer: {},
+  text: { fontWeight: "500", fontSize: fontSize.mid },
+  boldText: {
+    fontWeight: "bold",
+    fontSize: fontSize["3XLarge"],
+    marginTop: 10,
   },
 });
 MainDashboard.propTypes = {};
